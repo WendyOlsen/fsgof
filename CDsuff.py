6eece625b32eb8900e6ba83342854fcaaa9397f2
luminous-badger <luminous.badger@gmail.com>
2016-06-28 15:09:10 +0100

    Added Creative Commons licence. Removed commented code.

--- CDsuff.py
+++ CDsuff.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python
 
-# Print all combins of 1 from 6.
+# Contact developers John Mcloughlin and Wendy Olsen of the
+# Unversity of Manchester via the Facebook Group:
+# Integrated Mixed Methods Network.
+# This work is released under the Creative Commons Licence.
+# You are free to use, change and distribute this work as long
+# as you cite:
+# Fuzzy Set Goodness of Fit Tests Version 1.
+# JM & WO 2016/06/28
+# Requirements: Python modules numpy and matplotlib must be installed.
+# The Anaconda Python distribution contains all the required modules.
+# Download from: https://www.continuum.io/downloads
+# Usage - Windows Command Prompt: 
+# c:\python\python.exe CDsuff.py inputfile.csv Y-Value > outputfile.txt
+# where Y-Value is from 1 to 4. Eg
+# c:\python\python.exe CDsuff.py indiafile.csv 3 > outputfile.txt
+# Mac or Linux Terminal:
+# CDsuff.py inputfile.csv Y-Value > outputfile.txt
+# The path to the python executable is provided by the first line 
+# of the program. See line 1 above.
+# This program implements Ronggui Huang's 2010 R program for
+## Goodness-of-Fit Tests and Descriptive Measures in Fuzzy-Set Analysis
+## Eliason S. & Stryker R. 2009. Sociological Methods & Research 38:102-146.
+
+# Print all configurations involving X from 1 to 6.
 # Copy of cx.py with read of csv file added.
 # JM 2016/06/07
 # Copy of cxr.py.  
@@ -64,8 +87,7 @@ CDYval  : YNlist
 }
 
 def read_file():
-	# XvalMax is number of X columns to read.
-
+	
 	with open( fname, 'rb') as f:
 		reader = csv.reader(f)
 		for row in reader:
@@ -75,48 +97,48 @@ def read_file():
 				X0list.append( linelist[ 0 ] )
 				#print 'Xlist:', Xlist
 			except:
-				print 'XErr:',  linelist[ 0 ]
+				#print 'XErr:',  linelist[ 0 ]
 				pass
 			try:
 				X1list.append( float( linelist[ 1 ] ) )
 				#print 'Xlist:', Xlist
 			except:
-				print 'XErr:',  linelist[ 1 ]
+				#print 'XErr:',  linelist[ 1 ]
 				pass
 			try:
 				X2list.append( float( linelist[ 2 ] ) )
 				#print 'Xlist:', Xlist
 			except:
-				print 'XErr:',  linelist[ 2 ]
+				#print 'XErr:',  linelist[ 2 ]
 				pass
 			try:
 				X3list.append( float( linelist[ 3 ] ) )
 				#print 'Xlist:', Xlist
 			except:
-				print 'XErr:',  linelist[ 3 ]
+				#print 'XErr:',  linelist[ 3 ]
 				pass
 			try:
 				X4list.append( float( linelist[ 4 ] ) )
 				#print 'Xlist:', Xlist
 			except:
-				print 'XErr:',  linelist[ 4 ]
+				#print 'XErr:',  linelist[ 4 ]
 				pass
 			try:
 				X5list.append( float( linelist[ 5 ] ) )
 				#print 'Xlist:', Xlist
 			except:
-				print 'XErr:',  linelist[ 5 ]
+				#print 'XErr:',  linelist[ 5 ]
 				pass
 			try:
 				X6list.append( float( linelist[ 6 ] ) )
 				#print 'Xlist:', Xlist
 			except:
-				print 'XErr:',  linelist[ 6 ]
+				#print 'XErr:',  linelist[ 6 ]
 				pass			
 			try:
 				YNlist.append( float( linelist[ CDYval ] ) )
 			except:
-				print 'Yerr:', linelist[ CDYval ]
+				#print 'Yerr:', linelist[ CDYval ]
 				pass
 
 #************** Dsuff Processing ************** 
@@ -134,16 +156,9 @@ def plot_zt_graph( xlist = [], ylist = [], pltitle = 'DXY', fname = 'DXY.png' ):
 	plt.xlabel( 'ZX',fontsize=reqfontsize )
 	plt.ylabel( 'ZY' + str( Yval ),fontsize=reqfontsize )
 
-	#print 'XL:', xlist
-	#print 'YL:', ylist
 	plt.plot( [ Xmin, Xmax ], [ Ymin, Ymax ], '--', lw=2 )
 	plt.plot( [ xlist ], [ ylist ], 'rD', markersize=5 )
 	
-	#plt.plot( [ Xmin, Xmax ], [ Ymin, Ymax ], 'k-', lw=2 )
-
-	#plt.show( )
-	#print 'Fname:', fname
-	#print 'Dsuff:', Dsuff
 	plt.savefig( fname )
 	plt.close()
 	# Need to close to free up memory. Stops next slide being overwritten with previous slide data.
@@ -154,17 +169,17 @@ def Ztransform( zlist =[] ):
 			zlist[ ZL ] = damping_factor
 		elif ( zlist[ ZL ] > 1 - damping_factor ):
 			zlist[ ZL ] = 1 - damping_factor
-		#print 'ZT - ZL:', zlist[ ZL ] 
+		
 	qzxlist = norm.ppf( zlist )
 	return qzxlist
 
 def calc_df1(  xlist, ylist ):
 	df1 = 0.0
 	for XL in range(0, len( xlist ), 1 ):
-		#print 'DF1 - XL:', XL, 'Y:', ylist[ XL ], 'X:', xlist[ XL ]
+		
 		if ( ylist[ XL ] < xlist[ XL ] ):
 			df1 += 1
-			#print 'DF1 - YL:', ylist[ XL ], 'XL:', xlist[ XL ]
+		
 	return df1 		
 		
 def calc_ssd(  xlist, ylist, pltitle = 'DXY', fname = 'DXY.png' ):
@@ -172,27 +187,31 @@ def calc_ssd(  xlist, ylist, pltitle = 'DXY', fname = 'DXY.png' ):
 	zxlist = Ztransform( xlist )
 	zylist = Ztransform( ylist )
 	plot_zt_graph( zxlist, zylist, pltitle, fname )
-	#print 'SSD - ZX:', zxlist
-	#print 'SSD - ZY:', zylist
-	#print 'SSD - LZX:', len( zxlist )
-	#print 'SSD - LZY:', len( zylist )
+	
 	for XL in range(0, len( xlist ), 1 ):
-		#print 'SSD - XL:', XL
+		
 		if ( ylist[ XL ] > xlist[ XL ] ):
 			d = 1
 		else:
 			d = 0
 		
 		ssd += ( 1 - d ) *  ( zylist[ XL ] - zxlist[ XL ] )**2 
-		#print 'SSD - D:', d, 'YL:', zylist[ XL ], 'XL:', zxlist[ XL ], 'ssd:', ssd
+		
 	return ssd 
 
-def calc_nullsdZ( xlist, ylist, error_value ):
+def calc_nullsd3( xlist, ylist, error_value ):	
+# Use different calculation method for nullsd
+	nullsd = 0.0
+	df2 = len( ylist )
+	nullsd = df2 * error_value**2
+	return nullsd
+	
+def calc_nullsd2( xlist, ylist, error_value ):
 # Does calc on Z-Transformed xlist.
 	nullsd = 0.0
 	zxlist = Ztransform( xlist )
 	for XL in range(0, len( xlist ), 1 ):
-		#print 'SSD - XL:', XL
+		
 		if ( ylist[ XL ] > xlist[ XL ] ):
 			S = 1
 		else:
@@ -200,7 +219,7 @@ def calc_nullsdZ( xlist, ylist, error_value ):
 		nullsd += ( S *( 2 * error_value - 2 * error_value * zxlist[ XL ] ) + ( 1 - S ) * ( 2 * error_value * zxlist[ XL ] ) )**2
 	return nullsd
 	
-def calc_nullsd( xlist, ylist, error_value ):
+def calc_nullsd1( xlist, ylist, error_value ):
 	nullsd = 0.0
 	for XL in range(0, len( xlist ), 1 ):
 		#print 'SSD - XL:', XL
@@ -223,8 +242,8 @@ def proc_Dsuff(  xlist, ylist, pltitle = 'DXY', Csuff = 0.0, fname = 'DXY.png' )
 	
 	df2 = len( ylist )
 
-	nullsd = calc_nullsdZ( xlist, ylist, error_value )
-	emsd = nullsd / df2
+	nullsd = calc_nullsd3( xlist, ylist, error_value )
+	emsd = nullsd 
 	if ( df1 > 0 ):
 		msd = ssd/df1
 		F = msd/emsd 
@@ -245,17 +264,18 @@ def proc_Dsuff(  xlist, ylist, pltitle = 'DXY', Csuff = 0.0, fname = 'DXY.png' )
 	print 'PVAL - SF:', PVAL
 	print 'Csuff:', Csuff
 	'''
-	#print 
+	
 	ProcLabel = os.path.splitext( fname )[0] 
+	ProcLabel = ProcLabel.replace( 'D', '' ) # Remove D from Label.
 	print '{:>10s}'.format( ProcLabel ), 
 	print '{:>2d}'.format( Yval ),
 	print '{:>4.3f}'.format( Csuff ),
-	print '{:>6.3f}'.format( ssd ),
+	print '{:>8.3f}'.format( ssd ),
 	print '{:>11.3f}'.format( F ),
 	print '{:>3.2f}'.format( PVAL ),
 	print '{:>3d}'.format( df2 )
 	#print
-	opcsv.writerow( [ ProcLabel, Yval, '{:>4.3f}'.format( Csuff ), '{:>6.3f}'.format( ssd ), 
+	opcsv.writerow( [ ProcLabel, Yval, '{:>4.3f}'.format( Csuff ), '{:>8.3f}'.format( ssd ), 
 	'{:>11.3f}'.format( F ), '{:>3.2f}'.format( PVAL ), '{:>3d}'.format( df2 ) ] )
 
 #************** Csuff Processing ************** 
@@ -287,31 +307,21 @@ def plot_graph( xlist = [], ylist = [], pltitle = 'XY', Csuff = 0.0, fname = 'XY
 	
 	plt.plot( [ Xmin, Xmax ], [ Ymin, Ymax ], 'k-', lw=2 )
 
-	#plt.show( )
-	#print 'Fname:', fname
-	#print 'Csuff:', Csuff
 	plt.savefig( fname )
 	plt.close()
 	# Need to close to free up memory. Stops next slide being overwritten with previous slide data.
 
 
 def proc_cons6(  XvalListn = [ ] ):
-	#print 'PC6:', XvalListn, 'LX:', len( XvalListn )
+
 	x1,x2,x3,x4,x5,x6 = XvalListn
-	#print 'A,B,C,X,Y,Z:', x1,x2,x3,x4,x5,x6
+
 	xlist_plot = []
 	Csuff    = 0.0 
 	CsuffNum = 0.0 # Csuff Numerator
 	CsuffDen = 0.0 # Csuff Denominator
 	fname = 'X' + str( x1 ) + str( x2 ) + str( x3 ) + str( x4 ) + str( x5 ) + str( x6 ) + 'Y' + str( Yval ) + '.png'
 	for LCD in range( 0, len( column_dict[ x1 ] ), 1 ):
-		#print 'LCD:', LCD
-		#print 'ForX1:', column_dict[ x1 ][ LCD ] 
-		#print 'ForX2:', column_dict[ x2 ][ LCD ] 
-		#print 'ForX3:', column_dict[ x3 ][ LCD ] 
-		#print 'ForX4:', column_dict[ x4 ][ LCD ] 
-		#print 'ForX5:', column_dict[ x5 ][ LCD ] 
-		#print 'ForX6:', column_dict[ x6 ][ LCD ] 
 		CsuffNum +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ], 
 				  column_dict[ x4 ][ LCD ], column_dict[ x5 ][ LCD ], column_dict[ x6 ][ LCD ], column_dict[ CDYval ][ LCD ] ) 
 		CsuffDen +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ],
@@ -329,20 +339,15 @@ def proc_cons6(  XvalListn = [ ] ):
 	proc_Dsuff( xlist_plot, column_dict[ CDYval ], pltitle, Csuff, fname )
 
 def proc_cons5(  XvalListn = [ ] ):
-#	print 'PC5:', XvalListn, 'LX:', len( XvalListn )
+
 	x1,x2,x3,x4,x5 = XvalListn
-	#print 'A,B,X,Y,Z:', x1,x2,x3,x4,x5
+
 	xlist_plot = []
 	Csuff    = 0.0 
 	CsuffNum = 0.0 # Csuff Numerator
 	CsuffDen = 0.0 # Csuff Denominator
 	fname = 'X' + str( x1 ) + str( x2 ) + str( x3 ) + str( x4 ) + str( x5 )+ 'Y' + str( Yval ) + '.png'
 	for LCD in range( 0, len( column_dict[ x1 ] ), 1 ):
-		#print 'ForX1:', column_dict[ x1 ][ LCD ] 
-		#print 'ForX2:', column_dict[ x2 ][ LCD ] 
-		#print 'ForX3:', column_dict[ x3 ][ LCD ] 
-		#print 'ForX4:', column_dict[ x4 ][ LCD ] 
-		#print 'ForX5:', column_dict[ x5 ][ LCD ] 
 		CsuffNum +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ], 
 				  column_dict[ x4 ][ LCD ], column_dict[ x5 ][ LCD ], column_dict[ CDYval ][ LCD ] ) 
 		CsuffDen +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ],
@@ -360,19 +365,15 @@ def proc_cons5(  XvalListn = [ ] ):
 	proc_Dsuff( xlist_plot, column_dict[ CDYval ], pltitle, Csuff, fname )
 
 def proc_cons4(  XvalListn = [ ] ):
-	#print 'PC4:', XvalListn, 'LX:', len( XvalListn )
+
 	x1,x2,x3,x4 = XvalListn
-	#print 'A,X,Y,Z:', x1,x2,x3,x4
+
 	xlist_plot = []
 	Csuff    = 0.0 
 	CsuffNum = 0.0 # Csuff Numerator
 	CsuffDen = 0.0 # Csuff Denominator
 	fname = 'X' + str( x1 ) + str( x2 ) + str( x3 ) + str( x4 ) + 'Y' + str( Yval ) + '.png'
 	for LCD in range( 0, len( column_dict[ x1 ] ), 1 ):
-		#print 'ForX1:', column_dict[ x1 ][ LCD ] 
-		#print 'ForX2:', column_dict[ x2 ][ LCD ] 
-		#print 'ForX3:', column_dict[ x3 ][ LCD ] 
-		#print 'ForX4:', column_dict[ x4 ][ LCD ] 
 		CsuffNum +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ], 
 				  column_dict[ x4 ][ LCD ], column_dict[ CDYval ][ LCD ] ) 
 		CsuffDen +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ],
@@ -392,18 +393,15 @@ def proc_cons4(  XvalListn = [ ] ):
 
 
 def proc_cons3(  XvalListn = [ ] ):
-	#print 'PC3:', XvalListn, 'LX:', len( XvalListn )
+
 	x1,x2,x3 = XvalListn
-	#print 'X,Y,Z:', x1,x2,x3
+
 	xlist_plot = []
 	Csuff    = 0.0 
 	CsuffNum = 0.0 # Csuff Numerator
 	CsuffDen = 0.0 # Csuff Denominator
 	fname = 'X' + str( x1 ) + str( x2 ) + str( x3 ) + 'Y' + str( Yval ) + '.png'
 	for LCD in range( 0, len( column_dict[ x1 ] ), 1 ):
-		#print 'ForX:', column_dict[ x1 ][ LCD ] 
-		#print 'ForY:', column_dict[ x2 ][ LCD ] 
-		#print 'ForZ:', column_dict[ x3 ][ LCD ] 
 		CsuffNum +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ], column_dict[ CDYval ][ LCD ] ) 
 		CsuffDen +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ] ) 
 		xlist_plot.append( min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ x3 ][ LCD ] ) )
@@ -419,7 +417,7 @@ def proc_cons3(  XvalListn = [ ] ):
 
 
 def proc_cons2(  XvalListn = [ ] ):
-	#print 'PC2List:', XvalListn, 'LX:', len( XvalListn )
+
 	x1,x2 = XvalListn
 	xlist_plot = []
 	Csuff    = 0.0 
@@ -427,8 +425,6 @@ def proc_cons2(  XvalListn = [ ] ):
 	CsuffDen = 0.0 # Csuff Denominator
 	fname = 'X' + str( x1 ) + str( x2 ) + 'Y' + str( Yval ) + '.png'
 	for LCD in range( 0, len( column_dict[ x1 ] ), 1 ):
-		#print 'ForX:', column_dict[ x1 ][ LCD ] 
-		#print 'ForY:', column_dict[ x2 ][ LCD ] 
 		CsuffNum +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ], column_dict[ CDYval ][ LCD ] ) 
 		CsuffDen +=  min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ] ) 
 		xlist_plot.append( min( column_dict[ x1 ][ LCD ], column_dict[ x2 ][ LCD ] ) )
@@ -444,9 +440,6 @@ def proc_cons2(  XvalListn = [ ] ):
 
 
 def proc_cons1(  XvalListn = [ ] ):
-	#print 'PC1:', XvalListn, 'LX:', len( XvalListn )
-	## Setting x to XvalListn doesn't work. Cos it's a tuple ???
-	## Must set x, = XvalListn as XvalListn is a tuple. Feature !!
 	x1, = XvalListn
 	xlist_plot = []
 	Csuff    = 0.0 
@@ -454,18 +447,12 @@ def proc_cons1(  XvalListn = [ ] ):
 	CsuffDen = 0.0 # Csuff Denominator
 	fname = 'X' + str( x1 ) + 'Y' + str( Yval ) + '.png'
 	for LCD in range( 0, len( column_dict[ x1 ] ), 1 ):
-		#print 'ForX:',  column_dict[ x1 ][ LCD ] 
-		#print 'ForY:',  column_dict[ CDYval ][ LCD ] 
-		#print 'MIN:', min( column_dict[ x1 ][ LCD ], column_dict[ CDYval ][ LCD ] )
-		#print 'ForX:',  column_dict[ XvalListn[ 0 ] ] 
 		CsuffNum +=  min( column_dict[ x1 ][ LCD ], column_dict[ CDYval ][ LCD ] )
 		CsuffDen +=  column_dict[ x1 ][ LCD ]
 		xlist_plot.append( column_dict[ x1 ][ LCD ] )
 
 	if ( CsuffDen != 0 ):
 		Csuff = CsuffNum / CsuffDen
-	#print 'xlist_plot:', xlist_plot 
-	#print 'ylist_plot:', column_dict[ CDYval ]
 	pltitle = 'Plot of Y' + str( Yval ) + ' & Minimum of X' + str( x1 ) + '; Csuff = ' + str( Csuff )
 
 	fname = 'X' + str( x1 ) + 'Y' + str( Yval ) + '.png'
@@ -476,9 +463,9 @@ def proc_cons1(  XvalListn = [ ] ):
 	proc_Dsuff( xlist_plot, column_dict[ CDYval ], pltitle, Csuff, fname )
 	
 def proc_cons(  XvalList = [ ] ):
-	#print 'List:', XvalList, 'LX:', len( XvalList)
+
 	for n in range( len( XvalList) ):
-		#print 'PC N:', n,'XVn:', XvalList[ n ], 'LenXn:', len( XvalList[ n ] )
+
 		if ( len( XvalList[ n ] ) == 1 ):
 			#print 'Call ONE'
 			proc_cons1(  XvalList[ n ] )
@@ -518,16 +505,16 @@ opcsv = csv.writer( open( OPCSVfile, 'wb' ) )
 
 
 # Headers for output.
-print '{:>10s}'.format( 'Label' ),
+print '{:>10s}'.format( 'Config' ),
 print '{:>2s}'.format( 'Y' ),
 print '{:^7s}'.format( 'Csuff' ),
-print '{:^6s}'.format( 'ssd' ),
+print '{:^8s}'.format( 'Dsuff' ),
 print '{:^10s}'.format( 'F' ),
 print '{:<5s}'.format( 'PVAL' ),
 print '{:<3s}'.format( 'Num' ),
 print
 
-opcsv.writerow( [ 'Label', 'Y', 'Csuff', 'SSD', 'F', 'PVAL', 'Num' ] )
+opcsv.writerow( [ 'Config', 'Y', 'Csuff', 'Dsuff', 'F', 'PVAL', 'Num' ] )
 
 for Xindex in range( 1, len(varlist) + 1 ):
 	XvalList = list( itertools.combinations( varlist, Xindex ) )
